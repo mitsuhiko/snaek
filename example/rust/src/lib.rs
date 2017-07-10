c9b5ebe151861b410b82dcb9d6739613f4165f4b
Armin Ronacher <armin.ronacher@active-4.com>
2017-07-10 11:13:22 +0200

    Added gitignore for example

--- example/rust/src/lib.rs
+++ example/rust/src/lib.rs
@@ -4,7 +4,23 @@ pub struct Point {
     pub y: f32,
 }
 
+#[repr(u32)]
+pub enum Foo {
+    A = 1,
+    B,
+    C
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn example_get_origin() -> Point {
     Point { x: 0.0, y: 0.0 }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn example_print_foo(foo: *const Foo) {
+    println!("{}", match *foo {
+        Foo::A => "a",
+        Foo::B => "b",
+        Foo::C => "c",
+    });
 }
